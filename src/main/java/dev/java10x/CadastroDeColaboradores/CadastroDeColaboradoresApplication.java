ad066159eb71babf700efb459a2a36d289611db3
igorf08 <igorfmaa@gmail.com>
2026-04-17 15:37:32 -0300

    Code boilerplate

---
+++ src/main/java/dev/java10x/CadastroDeColaboradores/CadastroDeColaboradoresApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeColaboradores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeColaboradoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeColaboradoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadastroDeColaboradores/CadastroDeColaboradoresApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeColaboradores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeColaboradoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
