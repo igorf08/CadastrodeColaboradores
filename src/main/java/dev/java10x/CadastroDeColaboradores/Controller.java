edf586ec37b3f4c047631afbaae5353e1cebbb90
igorf08 <igorfmaa@gmail.com>
2026-04-17 16:19:13 -0300

    feat: Configuração de SpringBoot e controller inicial

---
+++ src/main/java/dev/java10x/CadastroDeColaboradores/Controller.java
@@ -0,0 +1,16 @@
+package dev.java10x.CadastroDeColaboradores;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nesta rota.";
+    }
+
+}
