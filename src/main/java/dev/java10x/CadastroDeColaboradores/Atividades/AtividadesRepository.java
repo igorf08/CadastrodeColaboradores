a89e3fac199141e651e5beb8b847d0c5b19978bb
igorf08 <igorfmaa@gmail.com>
2026-04-21 18:10:27 -0300

    feat: Adição de repository para atividades

---
+++ src/main/java/dev/java10x/CadastroDeColaboradores/Atividades/AtividadesRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.CadastroDeColaboradores.Atividades;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AtividadesRepository extends JpaRepository<AtividadesModel, Long> {
+}
