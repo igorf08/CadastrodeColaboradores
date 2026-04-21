608b81d75b9722ba085a601915dcbbb9f644f288
igorf08 <igorfmaa@gmail.com>
2026-04-21 18:18:47 -0300

    feat: C Cria as rotas do controller de atividades com mock

---
+++ src/main/java/dev/java10x/CadastroDeColaboradores/Atividades/MissoesController.java
@@ -0,0 +1,30 @@
+package dev.java10x.CadastroDeColaboradores.Atividades;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+
+
+    @GetMapping("/listar")
+    public String listarMissoes() {
+        return "Missões listadas com sucesso.";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missão criada com sucesso.";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missão alterada com sucesso.";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missão deletar com sucesso.";
+    }
+
+}
