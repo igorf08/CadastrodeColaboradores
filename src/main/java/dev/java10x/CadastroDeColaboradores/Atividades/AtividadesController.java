0fa6fc7fbf14ae66a9f0d18ed2d93bd751148338
igorf08 <igorfmaa@gmail.com>
2026-04-26 15:43:34 -0300

    refactor: Refatora o AtividadesController para incluir ResponseEntity nas rotas, customizando assim a resposta às requisições

--- src/main/java/dev/java10x/CadastroDeColaboradores/Atividades/AtividadesController.java
+++ src/main/java/dev/java10x/CadastroDeColaboradores/Atividades/AtividadesController.java
@@ -1,5 +1,7 @@
 package dev.java10x.CadastroDeColaboradores.Atividades;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,29 +17,49 @@ public class AtividadesController {
     }
 
     @PostMapping("/criar")
-    public AtividadesDTO criarAtividade(@RequestBody AtividadesDTO atividade) {
-        return atividadesService.criarAtividade(atividade);
+    public ResponseEntity<?> criarAtividade(@RequestBody AtividadesDTO novaAtividade) {
+        AtividadesDTO atividade = atividadesService.criarAtividade(novaAtividade);
+        return ResponseEntity.ok(atividade);
     }
 
     @GetMapping("/listar")
-    public List<AtividadesDTO> listarAtividades() {
-        return atividadesService.listarAtividades();
+    public ResponseEntity<List<AtividadesDTO>> listarAtividades() {
+        List<AtividadesDTO> atividades = atividadesService.listarAtividades();
+        return ResponseEntity.ok(atividades);
     }
 
     @GetMapping("/listar/{id}")
-    public AtividadesDTO listarAtividadePorId(@PathVariable Long id) {
-        return atividadesService.listarAtividadePorId(id);
+    public ResponseEntity<?> listarAtividadePorId(@PathVariable Long id) {
+        AtividadesDTO atividade = atividadesService.listarAtividadePorId(id);
+        if (atividade != null) {
+            return ResponseEntity.ok(atividade);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não foi encontrada nenhuma atividade com ID " + id);
+        }
     }
 
 
     @PutMapping("/editar/{id}")
-    public AtividadesDTO editarAtividade(@PathVariable Long id, @RequestBody AtividadesDTO atividade) {
-        return atividadesService.editarAtividade(id, atividade);
+    public ResponseEntity<?> editarAtividade(@PathVariable Long id, @RequestBody AtividadesDTO atividadeEditada) {
+        AtividadesDTO atividade = atividadesService.editarAtividade(id, atividadeEditada);
+        if (atividade != null) {
+            return ResponseEntity.ok(atividade);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não foi encontrada nenhuma atividade com ID " + id);
+        }
     }
 
     @DeleteMapping("/deletar/{id}")
-    public void deletarAtividade(@PathVariable Long id) {
-        atividadesService.deletarAtividade(id);
+    public ResponseEntity<String> deletarAtividade(@PathVariable Long id) {
+
+        AtividadesDTO atividade = atividadesService.listarAtividadePorId(id);
+
+        if (atividade != null) {
+            atividadesService.deletarAtividade(id);
+            return ResponseEntity.ok("Sua atividade foi excluída.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não foi encontrada nenhuma atividade com ID " + id);
+        }
     }
 
 }
