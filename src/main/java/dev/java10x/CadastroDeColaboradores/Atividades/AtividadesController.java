5011dab0bf526ec7a76928324400c87a8547cded
igorf08 <igorfmaa@gmail.com>
2026-04-27 20:36:01 -0300

    doc: Adicionar Swagger na aplicação para documentação das rotas no controller de Atividades e Colaboradores

--- src/main/java/dev/java10x/CadastroDeColaboradores/Atividades/AtividadesController.java
+++ src/main/java/dev/java10x/CadastroDeColaboradores/Atividades/AtividadesController.java
@@ -1,5 +1,9 @@
 package dev.java10x.CadastroDeColaboradores.Atividades;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,18 +21,31 @@ public class AtividadesController {
     }
 
     @PostMapping("/criar")
+    @Operation(summary = "Criação de atividade",  description = "Esta rota é responsável por criar uma atividade.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Atividade criada com sucesso."),
+    })
     public ResponseEntity<AtividadesDTO> criarAtividade(@RequestBody AtividadesDTO atividade) {
         atividadesService.criarAtividade(atividade);
         return ResponseEntity.ok(atividade);
     }
 
     @GetMapping("/listar")
+    @Operation(summary = "Listagem de atividades",  description = "Esta rota é responsável por listar as atividades cadastradas.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Atividades listadas com sucesso."),
+    })
     public ResponseEntity<List<AtividadesDTO>> listarAtividades() {
         List<AtividadesDTO> atividades = atividadesService.listarAtividades();
         return ResponseEntity.ok(atividades);
     }
 
     @GetMapping("/listar/{id}")
+    @Operation(summary = "Listagem de atividade específica",  description = "Esta rota é responsável por listar uma atividade cadastrada através do ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Atividade listada com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Não foi encontrada nenhuma atividade com o ID informado."),
+    })
     public ResponseEntity<?> listarAtividadePorId(@PathVariable Long id) {
         AtividadesDTO atividade = atividadesService.listarAtividadePorId(id);
         if (atividade != null) {
@@ -39,7 +56,15 @@ public class AtividadesController {
     }
 
     @PutMapping("/editar/{id}")
-    public ResponseEntity<?> editarAtividade(@PathVariable Long id, @RequestBody AtividadesDTO atividadeDTO) {
+    @Operation(summary = "Edição de atividade",  description = "Esta rota é responsável por editar uma atividade cadastrada através do ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Atividade editada com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Não foi encontrada nenhuma atividade com o ID informado."),
+    })
+    public ResponseEntity<?> editarAtividade(
+            @Parameter(description = "Usuário informa o id no caminho da requisição.")  @PathVariable Long id,
+            @Parameter(description = "Usuário informa a atividade a ser atualizado no corpo da requisição.") @RequestBody AtividadesDTO atividadeDTO
+    ) {
         AtividadesDTO atividade = atividadesService.listarAtividadePorId(id);
         if(atividade != null) {
             atividadesService.editarAtividade(id, atividadeDTO);
--- src/main/java/dev/java10x/CadastroDeColaboradores/Colaboradores/ColaboradorController.java
+++ src/main/java/dev/java10x/CadastroDeColaboradores/Colaboradores/ColaboradorController.java
@@ -1,5 +1,9 @@
 package dev.java10x.CadastroDeColaboradores.Colaboradores;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +21,13 @@ public class ColaboradorController {
     }
 
     @PostMapping("/criar")
+    @Operation(summary = "Criação de colaborador", description = "Esta rota fica responsável pela criação de colaboradores")
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Colaborador criado com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro na criação do colaborador.")
+    })
+
     public ResponseEntity<String> criarColaborador(@RequestBody ColaboradoresDTO colaborador) {
         ColaboradoresDTO novoColaborador = colaboradorService.criarColaborador(colaborador);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -24,12 +35,22 @@ public class ColaboradorController {
     }
 
     @GetMapping("/listar")
+    @Operation(summary = "Listagem de todos os colaboradores", description = "Esta rota é responsável por listar todos os colaboradores cadastrados.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Colaboradores listados com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro na listagem dos colaboradores.")
+    })
     public ResponseEntity<List<ColaboradoresDTO>> listarColaboradores() {
         List<ColaboradoresDTO> colaboradores = colaboradorService.listarColaboradores();
         return ResponseEntity.ok(colaboradores);
     }
 
     @GetMapping("/listar/{id}")
+    @Operation(summary = "Listagem de um colaborador específico",  description = "Esta rota é responsável por listar um colaborador cadastrado através do ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Colaborador listado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Não foi encontrado nenhum colaborador com o ID informado.")
+    })
     public ResponseEntity<?> listarColaboradorPorId(@PathVariable Long id) {
 
         ColaboradoresDTO colaborador = colaboradorService.listarColaboradorPorId(id);
@@ -42,7 +63,15 @@ public class ColaboradorController {
     }
 
     @PutMapping("/editar/{id}")
-    public ResponseEntity<?> editarColaboradorPorId(@PathVariable Long id, @RequestBody ColaboradoresDTO colaboradorAtualizado) {
+    @Operation(summary = "Edição de colaborador",  description = "Esta rota é responsável por editar um colaborador cadastrado através do ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Colaborador editado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Não foi encontrado nenhum colaborador com o ID informado.")
+    })
+    public ResponseEntity<?> editarColaboradorPorId(
+            @Parameter(description = "Usuário informa o id no caminho da requisição.") @PathVariable Long id,
+            @Parameter(description = "Usuário informa o colaborador a ser atualizado no corpo da requisição.") @RequestBody ColaboradoresDTO colaboradorAtualizado
+    ) {
 
         ColaboradoresDTO colaborador = colaboradorService.editarColaborador(id, colaboradorAtualizado);
 
@@ -56,6 +85,11 @@ public class ColaboradorController {
     }
 
     @DeleteMapping("/deletar/{id}")
+    @Operation(summary = "Deleção de colaborador",  description = "Esta rota é responsável por deletar um colaborador cadastrado através do ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Colaborador deletado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Não foi encontrado nenhum colaborador com o ID informado.")
+    })
     public ResponseEntity<String> deletarColaborador(@PathVariable Long id) {
 
         ColaboradoresDTO colaborador = colaboradorService.listarColaboradorPorId(id);
